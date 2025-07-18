4dc62a0443b7df5be280a7791e2f3db831aa214f
TipDevs <tipdevs@gmail.com>
2025-07-18 19:44:02 +0100

    Create a module that create folders for list of todos

---
+++ src/modules/todo_folder.js
@@ -0,0 +1,59 @@
+import PubSub from "pubsub-js";
+class TodoFolder {
+    constructor(title) {
+        this.title = title;
+        this.todos = [];
+        this.id = Date.now();
+    }
+}
+const EVENT = {
+    SAVE_FOLDER: "save to-do folder to local storage",
+    DELETE_FOLDER: "delete to-do folder from local storage", 
+}
+const todoFolderStorage = (() => {
+    const key = "Project";
+    const getTodoFolder = () => {
+        return JSON.parse(localStorage.getItem(key)) || [];
+    }
+    const saveTodoFolder = (folders) => {
+    // Make sure folders is always an array
+    if (!Array.isArray(folders)) {
+        folders = [folders];
+    }
+        localStorage.setItem(key, JSON.stringify(folders));
+    };
+
+    PubSub.subscribe(EVENT.SAVE_FOLDER, (msg, folder) => {
+        const folders = getTodoFolder();
+        folders.push(folder);
+        saveTodoFolder(folders);
+    });
+    PubSub.subscribe(EVENT.DELETE_FOLDER, (msg, folderId) => {
+        const folders = getTodoFolder();
+        const filteredFolders = folders.filter(folder => folder.id !== folderId);
+        saveTodoFolder(filteredFolders);
+    })
+    return { getTodoFolder, saveTodoFolder };
+})();
+
+const FolderLogic = (() => {
+    const addNewFolder = (folder) => PubSub.publish(EVENT.SAVE_FOLDER, folder);
+    const deleteFolder = (folderId) => PubSub.publish(EVENT.DELETE_FOLDER, folderId);
+
+    const initDefaultFolder = () => {
+        const folders = todoFolderStorage.getTodoFolder();
+        let defaultFolder = folders.find(folder => folder.title === "Default");
+
+        if (!defaultFolder) {
+            defaultFolder = new TodoFolder("Default");
+            addNewFolder(defaultFolder);
+        }
+
+        return defaultFolder;
+    };
+
+    return { addNewFolder, deleteFolder, initDefaultFolder };
+})();
+
+// const defaultFolder = FolderLogic.initDefaultFolder();
+export { todoFolderStorage };
