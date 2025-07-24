33c92e7a72974f24dc8cee93f1fcaef0f9fd9357
TipDevs <tipdevs@gmail.com>
2025-07-24 23:30:41 +0100

    Import and Export: Importation of new functions and variables from other modules

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import "./styles/styles.css"
 import { TodoFolder } from "./modules/todo_folder.js";
 import { TodoList, priorities } from "./modules/todo_list.js";
 import { UiHandlerLogic } from "./modules/UiHandler.js";
+import { projectDisplayer, addNewFolderClickEvent } from "./modules/clickHandler.js";
+document.addEventListener("DOMContentLoaded", () => {
+    projectDisplayer();
+    addNewFolderClickEvent();
+    UiHandlerLogic().addProject();
+});
