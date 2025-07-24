e5d0bec0f266e0943a1a718a4f9502c699ff3351
TipDevs <tipdevs@gmail.com>
2025-07-24 23:26:39 +0100

    Refactor to avoid circular import

--- src/modules/UiHandler.js
+++ src/modules/UiHandler.js
@@ -2,66 +2,77 @@ import PubSub from "pubsub-js";
 import { todoFolderStorage } from "./todo_folder.js";
 import { todoListStorage } from "./todo_list.js";
 import tip_do from "../assets/images/tip.do.webp";
+import { CLICK_EVENTS } from "./clickHandler.js";
 const EVENTS = {
     folderTodoList: "Display todo lists in folder clicked",
     folderList: "Display folder lists"
 }
 const UIsubscribers = (() => {
-    // const folders = todoFolderStorage.getTodoFolder();
+    // Event that add projects to project display list
     PubSub.subscribe(EVENTS.folderList, (msg, folders) => {
         const projectList = document.querySelector('.project_list');
-        // const todoFolder = todoFolderStorage.getTodoFolder();
+        projectList.innerHTML = "";
         folders.forEach(folder => {
             const projectBTN = document.createElement("button");
-            projectBTN.innerHTML = `${folder.title} <i class="fa-regular fa-circle-xmark"></i>`;
-            projectList.appendChild(projectBTN);
-            console.log(folder.todos);
+            projectBTN.id = `${folder.id}`;
+            if (folder.title === "Default") {
+            projectBTN.innerHTML = `${folder.title}`;   
+            }
+            else {
+                projectBTN.innerHTML = `${folder.title} <i class="fa-regular fa-circle-xmark"></i>`;
+            }
+            projectList.appendChild(projectBTN); 
         });
     });
-    PubSub.subscribe(EVENTS.folderTodoList, (msg, todos) => {
-        const listContainer = document.querySelector('.lists');
-        if (todos === ![]) {
-            todos.forEach(todo => {
-                const listInfo = document.createElement('div');
-                listInfo.classList.add('lists_items');
-                listInfo.innerHTML = `
-                <input type="checkbox">
-                <div class="lists_info">
-                    <p id="title">${todo.title}</p>
-                    <p id="description">${todo.description}</p>
-                    <p id="due_date">${todo.dueDate}</p>
-                </div>
-                `;
-                listContainer.appendChild(listInfo);
-            });
-        }
 
-        else {
-            const displayImage = document.createElement('img');
-            displayImage.src = tip_do;
-            const displayMessage = document.createElement('p');
-            displayMessage.textContent = "Nothing to do here";
-            listContainer.innerHTML = "";
-            listContainer.classList.add("empty");
-            listContainer.appendChild(displayImage);
-            listContainer.appendChild(displayMessage);
-        }
-    })
+    // Event that display list of todos in a particular project user clicked
+    PubSub.subscribe(EVENTS.folderTodoList, (msg, folder) => {
+        const main = document.querySelector('main');
+        main.innerHTML = '';
+        const listContainer = document.createElement('div');
+        listContainer.classList.add('lists');
+            if (folder.todos.length > 0) {
+                folder.todos.map(todo => {
+                    const listExist = document.querySelector(`#${todo.id}`);
+                    if (listExist) listExist.remove();
+                    listContainer.innerHTML = `
+                    <div class="lists_items, with_aside" id="${todo.id}">
+                        <input type="checkbox">
+                        <div class="lists_info">
+                            <p id="${todo.title}">${todo.title}</p>
+                            <p id="description">${todo.description}</p>
+                            <p id="due_date">${todo.dueDate}</p>
+                        </div>
+                    </div>
+                    `;
+                    main.appendChild(listContainer);
+                    main.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-square-plus fa-2xl add_list ${todo.id}"></i>`);
+                });
+            }
+
+            else {
+                const displayImage = document.createElement('img');
+                displayImage.src = tip_do;
+                const displayMessage = document.createElement('p');
+                displayMessage.textContent = "Nothing to do here";
+                listContainer.innerHTML = "";
+                listContainer.classList.add("empty");
+                listContainer.appendChild(displayImage);
+                listContainer.appendChild(displayMessage);
+            }
+        });
 })();
-const UiHandlerLogic = (() => {
+
+// Logic that publishes events
+const UiHandlerLogic = () => {
     const folders = todoFolderStorage.getTodoFolder();
     const addProject = () => {
         PubSub.publish(EVENTS.folderList, folders);
     }
-    const showTodoListPerProject = () => {
-        const todos = folders.map(folder => {
-            return folder.todos;
-        })
-        PubSub.publish(EVENTS.folderTodoList, todos);
+    const showTodoListPerProjectClick = (folder) => {
+        PubSub.publish(EVENTS.folderTodoList, folder);
     };
-    return { addProject, showTodoListPerProject };
-})()
-UiHandlerLogic.addProject();
-UiHandlerLogic.showTodoListPerProject();
-
+    return { addProject, showTodoListPerProjectClick };
+};
+// UiHandlerLogic.addProject();
 export { UiHandlerLogic };
