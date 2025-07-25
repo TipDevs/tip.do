da33136a400bac924746bb2dc921ef0b06680b49
TipDevs <tipdevs@gmail.com>
2025-07-26 00:18:51 +0100

    Ensure invoked correctly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles/styles.css"
-import { TodoFolder } from "./modules/todo_folder.js";
+import { TodoFolder, todoFolderStorage } from "./modules/todo_folder.js";
 import { TodoList, priorities } from "./modules/todo_list.js";
 import { UiHandlerLogic } from "./modules/UiHandler.js";
 import { projectDisplayer, addNewFolderClickEvent } from "./modules/clickHandler.js";
 document.addEventListener("DOMContentLoaded", () => {
+    todoFolderStorage();
     projectDisplayer();
     addNewFolderClickEvent();
     UiHandlerLogic().addProject();
