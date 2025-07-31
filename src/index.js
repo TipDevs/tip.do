b7a97e52a92a10ae5b1d6d44f067fe99a9d98b1e
TipDevs <tipdevs@gmail.com>
2025-07-31 12:53:41 +0100

    import new modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import "./styles/styles.css"
-import { TodoFolder, todoFolderStorage } from "./modules/todo_folder.js";
-import { TodoList, priorities } from "./modules/todo_list.js";
-import { UiHandlerLogic } from "./modules/UiHandler.js";
-import { projectDisplayer, addNewFolderClickEvent } from "./modules/clickHandler.js";
+import "./styles/styles.css";
+import PubSub from "pubsub-js";
+import { viewportTracker } from "./modules/viewportTracker";
+import { folderLogic, TaskFolder } from "./modules/project";
+import { taskLogic, Task } from "./modules/task";
+import { UiHandlerLogic, UI_EVENTS } from "./modules/UiHandler";
+import { displayTaskInProject } from "./modules/clickHandler";
+displayTaskInProject();
+viewportTracker();
 document.addEventListener("DOMContentLoaded", () => {
-    todoFolderStorage();
-    projectDisplayer();
-    addNewFolderClickEvent();
-    UiHandlerLogic().addProject();
+    UiHandlerLogic().showProject();
+    const projectTray = JSON.parse(localStorage.getItem("Project"));
+    projectTray.map(project => {
+        if (project.title === "Default") {
+            PubSub.publish(UI_EVENTS.displayTasks, project);
+        }
+    })
 });
