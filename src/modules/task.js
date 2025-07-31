7cc08736e8f1933680dea947ee1788759b6a9dec
TipDevs <tipdevs@gmail.com>
2025-07-31 12:46:02 +0100

    Ensure task update with UI in real time

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,13 +1,14 @@
 import PubSub from "pubsub-js";
-// // import { TaskFolder, FolderLogic } from "./Task_folder.js";
+import { UI_EVENTS } from "./UiHandler";
+// // import { Taskproject, projectLogic } from "./Task_project.js";
 // import { CLICK_EVENTS } from "./clickHandler.js";
 
 // Events and Priorities factory
 const setEventAndPriority = function () {
     const EVENTS = {
-        SAVE_Task: "save to-do to folder",
-        DELETE_Task: "delete to-do from folder",
-        EDIT_Task: "edit and update to-do in folder",
+        SAVE_Task: "save to-do to project",
+        DELETE_Task: "delete to-do from project",
+        EDIT_Task: "edit and update to-do in project",
     }
     const PRIORITIES = {
         high: "High",
@@ -30,54 +31,56 @@ class Task {
 
 // Task storage
 const taskListStorage = () => {
-    const getFolderTaskStorage = () => {
+    const getProjectTaskStorage = () => {
         return JSON.parse(localStorage.getItem("Project")) || [];
     }
-    const saveTaskToFolder = (newTask, folder) => {
-        folder.tasks.push(newTask);
-        // TaskFolderStorage().saveTaskFolder(folder);
+    const saveTaskToProject = (newTask, project) => {
+        project.tasks.push(newTask);
+        // TaskprojectStorage().saveTaskproject(project);
     }
 
-    return { getFolderTaskStorage, saveTaskToFolder };
+    return { getProjectTaskStorage, saveTaskToProject };
 };
 
 (function taskSubscribers() {
     // Subscribe to save Task event
-    PubSub.subscribe(setEventAndPriority().EVENTS.SAVE_Task, (msg, { task, folderId }) => {
-        const foldersTray = taskListStorage();
-        const folders = foldersTray.getFolderTaskStorage();
-        const updatedFolders = folders.map(folder => {
-            if (folder.id === folderId) {
-                foldersTray.saveTaskToFolder(task, folder);
+    PubSub.subscribe(setEventAndPriority().EVENTS.SAVE_Task, (msg, { task, projectId }) => {
+        const projectsTray = taskListStorage();
+        const projects = projectsTray.getProjectTaskStorage();
+        const updatedProjects = projects.map(project => {
+            if (project.id === projectId) {
+                projectsTray.saveTaskToProject(task, project);
+                PubSub.publish(UI_EVENTS.displayTasks, project);
             }
-            return folder;
-            // PubSub.publish(CLICK_EVENTS.show_task, folder);
+            return project;
+            // PubSub.publish(CLICK_EVENTS.show_task, project);
         });
-        localStorage.setItem("Project", JSON.stringify(updatedFolders));
+        localStorage.setItem("Project", JSON.stringify(updatedProjects));
     });
 
     // Subscribe to delete Task event
-    PubSub.subscribe(setEventAndPriority().EVENTS.DELETE_Task, (msg, { taskId, folderId }) => {
-        const foldersTray = taskListStorage();
-        const folders = foldersTray.getFolderTaskStorage();
-        const updatedFolders = folders.map(folder => {
-            if (folderId === folder.id) {
-                const updatedTasks = folder.tasks.filter(task => task.id !== taskId);
-                folder.tasks = updatedTasks;
+    PubSub.subscribe(setEventAndPriority().EVENTS.DELETE_Task, (msg, { taskId, projectId }) => {
+        const projectsTray = taskListStorage();
+        const projects = projectsTray.getProjectTaskStorage();
+        const updatedProjects = projects.map(project => {
+            if (projectId === project.id) {
+                const updatedTasks = project.tasks.filter(task => task.id !== taskId);
+                project.tasks = updatedTasks;
+                PubSub.publish(UI_EVENTS.displayTasks, project);
             }
-            return folder;
+            return project;
         });
-        localStorage.setItem("Project", JSON.stringify(updatedFolders));
+        localStorage.setItem("Project", JSON.stringify(updatedProjects));
         // PubSub.publish(CLICK_EVENTS.update_project);
     });
 
     // Subscribe to edit Task event
-    PubSub.subscribe(setEventAndPriority().EVENTS.EDIT_Task, (msg, { newTitle, newDescription, newPriority = setEventAndPriority.PRIORITIES.medium, newDueDate, taskId, folderId }) => {
-        const foldersTray = taskListStorage();
-        const folders = foldersTray.getFolderTaskStorage();
-        const updatedFolders = folders.map(folder => {
-            if (folder.id === folderId) {
-                const updatedTasks = folder.tasks.map(task => {
+    PubSub.subscribe(setEventAndPriority().EVENTS.EDIT_Task, (msg, { newTitle, newDescription, newPriority = setEventAndPriority.PRIORITIES.medium, newDueDate, taskId, projectId }) => {
+        const projectsTray = taskListStorage();
+        const projects = projectsTray.getProjectTaskStorage();
+        const updatedProjects = projects.map(project => {
+            if (project.id === projectId) {
+                const updatedTasks = project.tasks.map(task => {
                 if (taskId === task.id) {
                     task.title = newTitle;
                     task.description = newDescription;
@@ -86,11 +89,12 @@ const taskListStorage = () => {
                 }
                 return task;
                 });
-                folder.tasks = updatedTasks
+                project.tasks = updatedTasks
+                PubSub.publish(UI_EVENTS.displayTasks, project);
             }
-            return folder;
+            return project;
         });
-        localStorage.setItem("Project", JSON.stringify(updatedFolders));
+        localStorage.setItem("Project", JSON.stringify(updatedProjects));
         // PubSub.publish(CLICK_EVENTS.update_project);
     });
 })();
@@ -98,11 +102,11 @@ const taskListStorage = () => {
 const taskLogic = () => {
     const addTask = (newTask, containerId) => {
         console.log(`New Task: ${newTask}\n Container Id: ${containerId}`);
-        PubSub.publish(setEventAndPriority().EVENTS.SAVE_Task, { task: newTask, folderId: containerId});
+        PubSub.publish(setEventAndPriority().EVENTS.SAVE_Task, { task: newTask, projectId: containerId});
     };
-    const deleteTask = (taskId, containerId) => PubSub.publish(setEventAndPriority().EVENTS.DELETE_Task, { taskId: taskId, folderId: containerId });
+    const deleteTask = (taskId, containerId) => PubSub.publish(setEventAndPriority().EVENTS.DELETE_Task, { taskId: taskId, projectId: containerId });
     const editTask = (newTitle, newDescription, newPriority, newDueDate, taskId, containerId) => {
-        PubSub.publish(setEventAndPriority().EVENTS.EDIT_Task, { newTitle: newTitle, newDescription: newDescription, newPriority: newPriority, newDueDate: newDueDate, taskId: taskId, folderId: containerId});
+        PubSub.publish(setEventAndPriority().EVENTS.EDIT_Task, { newTitle: newTitle, newDescription: newDescription, newPriority: newPriority, newDueDate: newDueDate, taskId: taskId, projectId: containerId});
     }
     return { addTask, deleteTask, editTask };
 };
