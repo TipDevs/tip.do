f3bb478e64934db30738c35788cfb9d75f6f9cbd
TipDevs <tipdevs@gmail.com>
2025-08-03 08:33:01 +0100

    Ensure form rendering and cancellation works as intended
    Removal of unnecessary line of code was carried out

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import "./styles/styles.css";
 import PubSub from "pubsub-js";
 import { viewportTracker } from "./modules/viewportTracker";
-import { folderLogic, TaskFolder } from "./modules/project";
-import { taskLogic, Task } from "./modules/task";
-import { UiHandlerLogic, UI_EVENTS, usernameUI } from "./modules/UiHandler";
-import { displayTaskInProject } from "./modules/clickHandler";
-displayTaskInProject();
+import { UI_EVENTS, } from "./modules/UiHandler";
+import { CLICK_EVENTS,} from "./modules/clickHandler";
 viewportTracker();
 document.addEventListener("DOMContentLoaded", () => {
-    usernameUI();
-    UiHandlerLogic().showProject();
+    PubSub.publish(CLICK_EVENTS.update_project);
+    PubSub.publish(UI_EVENTS.updateUsername);
     const projectTray = JSON.parse(localStorage.getItem("Project"));
     projectTray.map(project => {
         if (project.title === "Default") {
-            PubSub.publish(UI_EVENTS.displayTasks, project);
+            PubSub.publish(CLICK_EVENTS.show_task, project);
         }
     })
 });
